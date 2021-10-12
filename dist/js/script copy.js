2c641e33672e6ab9d7ea1912109683d4b0022b77
Alina Gorbunova <alinagorbunova98@mail.ru>
2021-10-12 14:49:51 +0500

    Added tours pages and diffrent reviews

--- dist/js/script copy.js
+++ dist/js/script copy.js
@@ -1,144 +1,76 @@
+"use strict";
+
+import { animate } from "./animationScript.js";
+import { promoSlider } from "./promoSlider.js";
+import { aboutSlider } from "./aboutSlider.js";
+import { toursSlider } from "./toursSlider.js";
+import { reviewsSlider } from "./reviewsSlider.js";
+import { menuInit } from "./menuScript.js";
+
 const doc = document;
 
-const slidesWrapper = doc.querySelector('.promo__carousel'),
-      slidesField = doc.querySelector('.promo__carousel__inner'),
-      width = window.getComputedStyle(slidesField).width,
-      slides = doc.querySelectorAll('.slide'),
-      next = doc.querySelector('.next'),
-      prev = doc.querySelector('.prev');
-
-const descrWrapper = doc.querySelector('.promo__descr'),
-      descrs = doc.querySelectorAll('.slide-descr');
-
-let slider = [];
-for (let i = 0; i < slides.length; i++) {
-    slider[i] = slides[i].src;
-    slides[i].remove();
-}
-console.log(slider);
-
-let step = 0, 
-    offset = -1;
-
-function draw() {
-    let img = doc.createElement('img');
-    img.src = slider[step];
-    img.classList.add('slide');
-    img.style.left = offset * 1440 + 'px';
-    slidesField.appendChild(img);
-    if (step + 1 == slider.length){
-        step = 0;
-    } else {
-        step++;
+doc.addEventListener("DOMContentLoaded", function() {
+    const screenWidth = window.screen.width;
+    promoInit();
+    menuInit();
+    animate();
+    aboutSlider();
+    toursSlider();
+    if (screenWidth >= 1200) {
+        teamInit();
     }
-    if (offset != 1){
-        offset++;
-    }
-}
+    reviewsSlider();
 
-function draw2() {
-    let img = doc.createElement('img');
-    img.src = slider[step];
-    img.classList.add('slide');
-    img.style.left = -1440 + 'px';
-    slidesField.insertBefore(img, slidesField.firstChild);
-
-    if (step + 1 == slider.length){
-        step = 0;
-    } else {
-        step++;
-    }
-    if (offset != 1){
-        offset++;
-    }
-}
-
-function left() {
-    next.removeEventListener('click', left);
-    let slides2 = doc.querySelectorAll('.slide');
-    let offset2 = -1;
-    for (let i = 0; i < slides2.length; i++){
-        slides2[i].style.left = offset2 * 1440 - 1440 + 'px';
-        offset2++;
-    }
-    setTimeout(function(){
-        slides2[0].remove();
-        draw();
-        next.addEventListener('click', left);
-    }, 1000);
-}
-
-function right() {
-    prev.removeEventListener('click', right);
-    let slides2 = doc.querySelectorAll('.slide');
-    let offset2 = 1;
-    for (let i = 0; i < slides2.length; i++){
-        slides2[i].style.left = offset2 * 1440 - 1440 + 'px';
-        console.log(slides2[i].src);
-        console.log(offset2 * 1440 - 1440 + 'px');
-        offset2++;
-    }
-    setTimeout(function(){
-        slides2[2].remove();
-        draw2();
-        prev.addEventListener('click', right);
-    }, 1000);
-}
-
-next.addEventListener('click', left);
-prev.addEventListener('click', right);
-
-draw(); draw(); draw();
-      /* descrField.style.width = 100 * slides.length + '%';
-      descrs.forEach(slide => {
-        slide.style.width = width;
-      });
-
-      slidesField.style.width = 100 * slides.length + '%';
-      slides.forEach(slide => {
-        slide.style.width = width;
-      });
-
-      let slideIndex = 1;
-      let offset = 0;
-
-      next.addEventListener('click', () => {
-          if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)){
-              offset = 0;
-          } else {
-              offset += +width.slice(0, width.length - 2);
-          }
-          descrField.style.transform = `translateX(-${offset}px)`;
-          slidesField.style.transform = `translateX(-${offset}px)`;
-          setTimeout(() => {  
-            if (offset == +width.slice(0, width.length - 2)){
-                let slide = document.createElement("div");
-                slide.innerHTML = slides[0].innerHTML;
-                slide.classList.add('slide');
-                console.log(slide);
-                slide.style.width = width;
-                slidesField.appendChild(slide);
-                slides[0].remove();
-                offset -= +width.slice(0, width.length - 2);
-                slidesField.style.transform = `translateX(-${offset}px)`;
-              }
-           }, 2000);
-          if (offset == +width.slice(0, width.length - 2)){ //DERRER
-            let slide = document.createElement("div");
-            slide.innerHTML = slides[0].innerHTML;
-            slide.classList.add('slide');
-            console.log(slide);
-            slide.style.width = width;
-            slidesField.appendChild(slide);
-            slides[0].remove(); //RFEFEWGG
-          }
-      });
-
-      prev.addEventListener('click', () => {
-        if (offset == 0 ) {
-            offset == +width.slice(0, width.length - 2) * (slides.length - 1);
+    const pageup = doc.querySelector('.pageup');
+    window.addEventListener('scroll', function(e) {
+        if (this.scrollY > 1000) {
+            pageup.style.display = 'block';
         } else {
-            offset -= +width.slice(0, width.length - 2);
+            pageup.style.display = 'none';
         }
-        slidesField.style.transform = `translateX(-${offset}px)`;
-    }); */
+    });
+
+    const smoothLinks = document.querySelectorAll('a[href^="#"]');
+    for (let smoothLink of smoothLinks) {
+        smoothLink.addEventListener('click', (e) => {
+            e.preventDefault();
+            const id = smoothLink.getAttribute('href');
+
+            doc.querySelector(id).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    };
+
+/*     const btnApplication = doc.querySelector('.btn_application');
+    btnApplication.addEventListener('click', (e)=>{
+        e.preventDefault();
+        window.location.href = '#application';
+    });
+ */
+});
+
+function promoInit() {
+    const menu = doc.querySelector('nav'),
+          logo = doc.querySelector('.promo__logo'),
+          logoContainerText = doc.querySelector('.promo__logo-container-text');
+
+    logo.classList.add('active');
+    menu.classList.add('active');
+    logoContainerText.classList.add('active');
+
+    promoSlider();
+}
+
+function teamInit() {
+    const teamPhotos = doc.querySelectorAll('.team__slider-photo');
+    teamPhotos.forEach(item=> {
+        item.addEventListener('mouseover', ()=>{
+            item.nextElementSibling.style.maxHeight = '300px';
+        });
+        item.addEventListener('mouseout', ()=>{
+            item.nextElementSibling.style.maxHeight = '70px';
+        });
+    });
+}
---
+++ src/js/scriptTours.js
@@ -0,0 +1,89 @@
+'use strict';
+
+const doc = document;
+const chapters = doc.querySelectorAll('.program__content');    
+const chaptersList = doc.querySelectorAll('.program__list > li');
+
+doc.addEventListener("DOMContentLoaded", function() {
+    
+    chaptersList.forEach(function(item, i) {
+        item.addEventListener('click', (e)=>{
+            e.preventDefault();
+            changeContentChapter(i);
+            item.classList.add('active');
+        });
+    });
+
+    const currencyItems = doc.querySelectorAll('.currency__item');
+    const currency = {};
+
+    axios.get("https://www.cbr-xml-daily.ru/daily_json.js")
+    .then((res) => {
+        currency.KZT = res.data.Valute.KZT.Value;
+        currency.USD = res.data.Valute.USD.Value;
+        setDataCurrency(res.data.Date);
+    });
+
+    const currencyPrices = doc.querySelectorAll('.currency-price');
+    const currencyFirstPrices = [];
+
+    currencyPrices.forEach(price => {
+        const str = price.innerHTML.replace(/\s/g, '');
+        currencyFirstPrices.push(str);
+    });
+
+    currencyItems.forEach(function(item, i) {
+        item.addEventListener('click', () => {
+            currencyItems.forEach(item => {
+                item.classList.remove('active');
+            });
+            item.classList.add('active');
+            changeCurrency(currency, currencyPrices, currencyFirstPrices, i);
+        });
+    });
+
+});
+
+function setDataCurrency(date) {
+    const currencyDate = {Year: '', Month: '', Day: ''};
+    currencyDate.Year = date.substr(0,4);
+    currencyDate.Month = date.substr(5,2);
+    currencyDate.Day = date.substr(8,2);
+    
+    const dateText = doc.querySelector('.currency__date');
+    dateText.innerHTML += ' ' + currencyDate.Day + '.' + currencyDate.Month + '.' + currencyDate.Year;
+}
+
+function changeCurrency(currency, currencyPrices, currencyFirstPrices, index) {
+    if (index === 0) {
+        currencyPrices.forEach(function(price, i) {
+            price.innerHTML = currencyFirstPrices[i].toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+        });
+    } else if (index === 1) {
+        currencyPrices.forEach(function(price, i) {
+            const newPrice = Math.round(currencyFirstPrices[i] * currency.USD / currency.KZT * 100);
+            price.innerHTML = newPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+            console.log(currency);
+        });
+    } else {
+        currencyPrices.forEach(function(price, i) {
+            const newPrice = Math.round(currencyFirstPrices[i] * currency.USD);
+            price.innerHTML = newPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+        });
+    }
+
+}
+
+function changeContentChapter(index) {
+    closeAllChapters();
+    chapters[index].style.display = 'block';
+}
+
+function closeAllChapters() {
+    chaptersList.forEach(item => {
+        item.classList.remove('active');
+    });
+    chapters.forEach(chapter => {
+        chapter.style.display = 'none';
+    });
+}
